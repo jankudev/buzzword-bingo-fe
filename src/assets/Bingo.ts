b211d1615ec7e47730b2a805b4af5344b349ec16
Janku Tomas (cen87430) <TJanku@csas.cz>
2019-12-06 16:37:41 +0100

    Bingo grid-list styling responsive

--- src/assets/Bingo.ts
+++ src/assets/Bingo.ts
@@ -6,10 +6,10 @@ export class BingoTile {
 export class Bingo {
   readonly cols = 5;
   readonly tiles: BingoTile[] = [
-    { text: 'Agile'}, {text: 'Scrum'}, {text: 'Craft'}, {text: 'DevOps'}, {text: 'Future bank'},
-    { text: 'Agile'}, {text: 'Scrum'}, {text: 'Craft'}, {text: 'DevOps'}, {text: 'Future bank'},
-    { text: 'Agile'}, {text: 'Scrum'}, {text: 'Craft'}, {text: 'DevOps'}, {text: 'Future bank'},
-    { text: 'Agile'}, {text: 'Scrum'}, {text: 'Craft'}, {text: 'DevOps'}, {text: 'Future bank'},
-    { text: 'Agile'}, {text: 'Scrum'}, {text: 'Craft'}, {text: 'DevOps'}, {text: 'Future bank'}
+    {text: 'Agile'}, {text: 'Scrum'}, {text: 'Craft'}, {text: 'DevOps'}, {text: 'Future bank test test test'},
+    {text: 'Agile'}, {text: 'Scrum'}, {text: 'Craft'}, {text: 'DevOps'}, {text: 'Future bank'},
+    {text: 'Agile'}, {text: 'Scrum'}, {text: 'Craft'}, {text: 'DevOps'}, {text: 'Future bank'},
+    {text: 'Agile'}, {text: 'Scrum'}, {text: 'Craft'}, {text: 'DevOps'}, {text: 'Future bank'},
+    {text: 'Agile'}, {text: 'Scrum'}, {text: 'Craft'}, {text: 'DevOps'}, {text: 'Future bank'}
   ];
 }
