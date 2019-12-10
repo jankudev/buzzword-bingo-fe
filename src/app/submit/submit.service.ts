9c22bb75f984b66d485c70715381e1e6f378563c
Janku Tomas (cen87430) <TJanku@csas.cz>
2019-12-10 17:32:03 +0100

    cors headers

--- src/app/submit/submit.service.ts
+++ src/app/submit/submit.service.ts
@@ -8,7 +8,8 @@ export class SubmitService {
   readonly buzzwordBingoApiURL = 'https://buzzword-bingo-proxy.appspot.com/results';
   readonly httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
     })
   };
 
