8660f3eef65311edd6f1adfb5b1d72eb6736304b
Janku Tomas (cen87430) <TJanku@csas.cz>
2019-12-10 16:13:52 +0100

    test https over proxy on gcp

--- src/app/submit/submit.service.ts
+++ src/app/submit/submit.service.ts
@@ -5,7 +5,7 @@ import {v4 as uuid} from 'uuid';
 
 @Injectable()
 export class SubmitService {
-  readonly buzzwordBingoApiURL = 'https://34.69.136.241/results';
+  readonly buzzwordBingoApiURL = 'https://buzzword-bingo-proxy.appspot.com/results';
   readonly httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
